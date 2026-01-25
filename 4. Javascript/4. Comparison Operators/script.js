288e940fe65090a8dfd0b9b67b087e0ba90bb5b1
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-25 16:13:34 +0530

    comparison

---
+++ 4. Javascript/4. Comparison Operators/script.js
@@ -0,0 +1,35 @@
+let numberofApples = 10; //number
+let numberoforanges = 20; //number
+
+//! ==
+let isEqual = numberofApples == numberoforanges;
+// console.log(isEqual);
+
+//! ===
+
+let isStrictlyEqual = numberofApples === numberoforanges;
+// console.log(isStrictlyEqual);
+
+//! !=
+let isNotEqual = numberofApples != "10";
+// console.log(isNotEqual);
+
+//! !==
+let isStrictlyNotEqual = numberofApples !== "10";
+// console.log(isStrictlyNotEqual);
+
+//! >
+let isGreaterThan = numberofApples > numberoforanges;
+// console.log(isGreaterThan);
+
+//! <
+let isLessThan = numberofApples < numberoforanges;
+console.log(isLessThan);
+
+//! >=
+let greaterorEqual = numberofApples >= 10;
+console.log(greaterorEqual);
+
+//! <=
+let lessorequal = numberoforanges <= 15;
+console.log(lessorequal);
