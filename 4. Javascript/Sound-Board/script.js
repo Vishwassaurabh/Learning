5e87b7275bad4c925b0909775ff598557113b8aa
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-26 16:24:43 +0530

    sound Board

---
+++ 4. Javascript/Sound-Board/script.js
@@ -0,0 +1,15 @@
+document.getElementById("btn1").addEventListener("click", () => {
+  const audio = new Audio("audio/sound1.mp3");
+  audio.play();
+});
+
+document.getElementById("btn2").addEventListener("click", () => {
+  const audio = new Audio("audio/sound2.mp3");
+  audio.play();
+});
+
+document.getElementById("btn3").addEventListener("click", () => {
+  const audio = new Audio("audio/sound3.mp3");
+  audio.play();
+});
+
