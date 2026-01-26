6a328ea30374d9ec3cc030b4b9e8e7c1c58c91c3
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-27 00:57:54 +0530

    window-object

---
+++ 4. Javascript/10.DOM/2.window-object/script.js
@@ -0,0 +1,24 @@
+//----
+//window object
+//-----
+
+// console.log(window.innerWidth);
+console.log(innerHeight);
+// console.log(window.innerHeight);
+console.log(innerWidth);
+
+// console.log(window.location);
+console.log(location);
+
+// console.log(window.location.host);
+// console.log(window.history);
+// console.log(window.navigator);
+
+// console.log(window.screen);
+// console.log(window.alert());
+// console.log(window.confirm());
+
+// window.prompt();
+// window.open("https://google.com");
+
+console.log(document.body);
