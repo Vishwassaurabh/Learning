f2f0e91d994e95c5be8bedf7b63ba14772773852
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-27 15:07:37 +0530

    query-selector

---
+++ 4. Javascript/10.DOM/5.QuerySelector/script.js
@@ -0,0 +1,18 @@
+// get the product card
+
+const firstproductcard = document.querySelector("#product-card");
+console.log(firstproductcard);
+
+//get the highest-rated product
+const higestrating = document.querySelector("[data-rating='4.8']");
+console.log(higestrating);
+
+//get the first laptop category product
+const laptopcategory = document.querySelector("[data-category='laptop']");
+console.log(laptopcategory);
+
+//selecting an id
+
+const dashboard = document.querySelector("#dashboard");
+console.log(dashboard);
+
