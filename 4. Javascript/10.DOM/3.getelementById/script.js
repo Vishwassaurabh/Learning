90e34294dd737f7e8960f03756ae27232d5b0d13
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-27 14:12:02 +0530

    getelementbyid

---
+++ 4. Javascript/10.DOM/3.getelementById/script.js
@@ -0,0 +1,19 @@
+//----------------
+//selecting elements using getelementbyid
+//-------
+
+const title = document.getElementById("conferenceTitle");
+const date = document.getElementById("conferenceDate");
+const Locations = document.getElementById("locationTitle");
+const conferenceLocation = document.getElementById("conferenceLocation");
+const speakersTitle = document.getElementById("speakersTitle");
+const speakerList = document.getElementById("speakerList");
+
+console.log({
+  title,
+  date,
+  Locations,
+  conferenceLocation,
+  speakersTitle,
+  speakerList,
+});
