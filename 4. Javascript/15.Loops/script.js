2a0f8c59d7b7f8ee09399b95d58763c74b67d305
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-29 16:56:49 +0530

    loops

--- 4. Javascript/15.Loops/script.js
+++ 4. Javascript/15.Loops/script.js
@@ -2,8 +2,19 @@
 
 //* for(initializer, condition, increment/update){}
 
-
 //cont number from  1 to 10
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+//! while loops
+// ! initializer
+//! condition
+//! increment/update
+let count = 1; //initializer
+while (count <= 5) {
+  //inceriment.
+  console.log(count);
+  count++;
+  
 }
