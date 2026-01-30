45dd7607f55e6ded9871154f4b116c5e265860de
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-30 13:12:30 +0530

    objects

--- 4. Javascript/18.Data-sturcture/objects/script.js
+++ 4. Javascript/18.Data-sturcture/objects/script.js
@@ -11,6 +11,18 @@ person.country = "USA";
 person.isMarried = "false";
 
 //! Bracket notation
-person["job "] = "developer";
+person["job"] = "developer";
 person["courses"] = ["javascripts", "nodejs", "HTML"];
 console.log(person);
+
+//! Accessing object properties
+//? Dot Notation
+const personName = person.name;
+const personage = person.age;
+const personcourse = person.courses;
+console.log(personcourse);
+console.log(personage);
+
+//? Bracket Notation
+const personjob = person["job"];
+console.log(personjob);
