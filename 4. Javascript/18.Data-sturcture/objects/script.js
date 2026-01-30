96d25133f51c0d13729707e34dad5bbfef19eac7
Vishwas saurabh <techvishwasindia@gmail.com>
2026-01-30 12:36:58 +0530

    objects

---
+++ 4. Javascript/18.Data-sturcture/objects/script.js
@@ -0,0 +1,16 @@
+// creating ob jects
+const person = {
+  name: "john",
+  age: 30,
+  city: "New York",
+};
+
+// adding properties to object
+//! Dot notation
+person.country = "USA";
+person.isMarried = "false";
+
+//! Bracket notation
+person["job "] = "developer";
+person["courses"] = ["javascripts", "nodejs", "HTML"];
+console.log(person);
