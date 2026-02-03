20d03eb3e492fd8db6c2067552fce96bd575ef83
Vishwas saurabh <techvishwasindia@gmail.com>
2026-02-03 13:15:33 +0530

    Auto-typing

---
+++ 4. Javascript/23.Auto typing/script.js
@@ -0,0 +1,9 @@
+const type = new Typed(".auto-type", {
+  strings: ["Developer", "Designer", "Freelance", "Full Stack Developer",'Software Developer',],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  backDelay: 1000,
+  cursorChar: "||",
+  smartBackspace: true,
+});
