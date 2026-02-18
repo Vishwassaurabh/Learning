37ee2704badadce6a771e7ce1ddfec643cd6d817
Vishwas saurabh <techvishwasindia@gmail.com>
2026-02-18 16:34:17 +0530

    inline-event-handler

---
+++ 5.Javascript Advanced/15.inline-event-handlers/script.js
@@ -0,0 +1,41 @@
+//-----mouse event----
+function clickHandler() {
+  console.log("click event is called");
+}
+function dbClickHandler() {
+  console.log("dbclick event is called");
+}
+
+function onMouseOverHandler() {
+  console.log("OnMouseOverHandler event is called");
+}
+
+function onMouseOutHandler() {
+  console.log("onMouseOutHandler event is called");
+}
+
+//----------keyboard event------
+function onkeydownHandler() {
+  console.log("onkeydownHandler event is called");
+}
+
+function onkeyupHandler() {
+  console.log("onkeyupHandler event is called");
+}
+
+function onkeypressHandler() {
+  console.log("onkeypressHandler event is called");
+}
+
+//----------Form event-------------
+function onsubmitHandler() {
+  console.log("onsubmitHandler event is called");
+}
+
+function onfocusHandler() {
+  console.log("onfocusHandler event is called");
+}
+
+function onblurHandler() {
+  console.log("onblurHandler event is called");
+}
